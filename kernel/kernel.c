7fa83e5ef16b0b26d4e04f85c7d21cd2d1a34044
0xqookie <b456231@trbvn.com>
2017-01-31 23:30:56 +0100

    Simple letter printing

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,21 @@
 /* stub kernel that halts the system */
 
+//#include <stdint.h>
+
 asm(".org 0x100000");				// the kernel is loaded at 0x100000
 
+int pos;
+
 void main(void) {
-	asm("xchg bx, bx");
+	//asm("xchg bx, bx");
+	char* vidmem = (char*) 0xb8000;
+	//while (pos < 80 * 25) {
+//		vidmem[pos * 2] = 'A';
+		// vidmem[pos * 2 + 1] = 0x0F;
+		// pos ++;
+	// }
+	vidmem[0] = 'A';
+	vidmem[1] = 0x0F;
+
 	asm("hlt");
 }

ad0ed896d8c09ae28b71b2c2f7e5ae14f852c8e7
0xqookie <b456231@trbvn.com>
2017-02-01 15:48:46 +0100

    Added an putstr function. Doesnt boot

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,17 +1,34 @@
 /* stub kernel */
 
 #include "libs/stdint.h"
+#include "libs/string.h"
 
-void putchar(char c);
+void putchar(char c, char x, char y);
+void putstr(char* c, char x, char y);
 
 void _start(void) {
-	asm("xchg bx, bx");
-	putchar('a');
+	// asm("xchg bx, bx");
+	putstr("Testy test", 5, 4, 10);
 	asm("hlt");
 }
 
-void putchar(char c) {
+void putstr(const char* str, char x, char y, int len) {
+	//int len = strlen(str);
+	int i = 0;
+	while (i < len) {
+		putchar(str[i], x, y);
+		x++;
+		if (x > 80 - 1) {
+			x = 0;
+			y++;
+		}
+		i++;
+	}
+}
+
+void putchar(char c, char x, char y) {
 	char* vidmem = (char*) 0xb8000;
-	vidmem[0] = c;
-	vidmem[1] = 0x0F;
+	short pos = x + 80 * y; // position on 1D array
+	vidmem[pos * 2] = c; // video memor
+	vidmem[pos * 2 + 1] = 0x0F;
 }

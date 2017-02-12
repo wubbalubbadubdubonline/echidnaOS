1ea56b8134f1f75bf93374e6a4a31991f02e01cd
0xqookie <b456231@trbvn.com>
2017-02-12 17:20:21 +0100

    New ata driver

---
+++ kernel/drivers/ata.h
@@ -0,0 +1,29 @@
+#ifndef ATA_H
+#define ATA_H
+
+#include "../libs/stdint.h"
+#include "../libs/stddef.h"
+#include "system.h"
+
+typedef struct ata_device ata_device;
+
+struct ata_device {
+    uint8_t master; // this is a boolean
+    
+    uint16_t data_port;
+    uint8_t error_port;
+    uint8_t sector_count_port;
+    uint8_t lba_low_port;
+    uint8_t lba_mid_port;
+    uint8_t lba_hi_port;
+    uint8_t device_port;
+    uint8_t command_port;
+    uint8_t control_port;
+    
+    uint16_t bytes_per_sector;
+};
+
+ata_device init_ata_device(uint16_t port_base, uint8_t master);
+void ata_identify(ata_device dev);
+
+#endif
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,6 +5,7 @@
 #include "libs/string.h"
 #include "drivers/system.h"
 #include "drivers/textdrv.h"
+#include "drivers/ata.h"
 
 void _start(void) {
 
@@ -13,7 +14,27 @@ void _start(void) {
 	text_clear();
 
 	text_putchar(0x00);
-	text_putstring("echidnaOS\n\nhello world\b\naaaa\b");
+	text_putstring("echidnaOS\nCrispy with ATA support\n");
+        
+        ata_device ata0m; ata0m = init_ata_device(0x1F0,1); // Primary master
+        text_putstring("ATA primary master: ");
+        ata_identify(ata0m);
+        text_putchar('\n');
+        
+        ata_device ata0s; ata0s = init_ata_device(0x1F0,0); // Primary slave
+        text_putstring("ATA primary slave: ");
+        ata_identify(ata0s);
+        text_putchar('\n');
+        
+        ata_device ata1m; ata1m = init_ata_device(0x170,1); // Secondary master
+        text_putstring("ATA secondary master: ");
+        ata_identify(ata1m);
+        text_putchar('\n');
+        
+        ata_device ata1s; ata1s = init_ata_device(0x170,0); // Secondary slave
+        text_putstring("ATA secondary slave: ");
+        ata_identify(ata1s);
+        text_putchar('\n');
 
 	system_halt();
 

4974a4dae3fbb9963a2a9ab467fe7a7f53a6f393
Mintz98 <the.mintlander@gmail.com>
2017-02-12 18:06:52 +0100

    changing types of port io

--- kernel/drivers/system.h
+++ kernel/drivers/system.h
@@ -1,19 +1,17 @@
 #ifndef __SYSTEM_H__
 #define __SYSTEM_H__
 
-
+#include "../libs/stdint.h"
+#include "../libs/stddef.h"
 
 #define magic_breakpoint()		asm("xchg bx, bx");
 
 void system_halt(void);
-void port_out_b(int port, int value);
-void port_out_w(int port, int value);
-void port_out_d(int port, int value);
-int port_in_b(int port);
-int port_in_w(int port);
-int port_in_d(int port);
-
-
-
+void port_out_b(uint16_t port, uint8_t value);
+void port_out_w(uint16_t port, uint16_t value);
+void port_out_d(uint16_t port, uint32_t value);
+uint8_t port_in_b(uint16_t port);
+uint16_t port_in_w(uint16_t port);
+uint32_t port_in_d(uint16_t port);
 
 #endif
--- kernel/drivers/x86_64/system.c
+++ kernel/drivers/x86_64/system.c
@@ -4,46 +4,50 @@ void system_halt(void) {
 	asm volatile ("cli; hlt;");
 }
 
-void port_out_b(int port, int value) {
+void port_out_b(uint16_t port, uint8_t value) {
 	asm volatile (	"out dx, al;"
 					:
 					: "d" (port), "a" (value)
 					: );
 }
 
-void port_out_w(int port, int value) {
+void port_out_w(uint16_t port, uint16_t value) {
 	asm volatile (	"out dx, ax;"
 					:
 					: "d" (port), "a" (value)
 					: );
 }
 
-void port_out_d(int port, int value) {
+void port_out_d(uint16_t port, uint32_t value) {
 	asm volatile (	"out dx, eax;"
 					:
 					: "d" (port), "a" (value)
 					: );
 }
 
-int port_in_b(int port) {
-	asm volatile (	"xor eax, eax;"
-					"in al, dx;"
-					:
+uint8_t port_in_b(uint16_t port) {
+	uint8_t value;
+	asm volatile (	"in al, dx;"
+					: "=a" (value)
 					: "d" (port)
 					: );
+	return value;
 }
 
-int port_in_w(int port) {
-	asm volatile (	"xor eax, eax;"
-					"in ax, dx;"
-					:
+uint16_t port_in_w(uint16_t port) {
+	uint16_t value;
+	asm volatile (	"in ax, dx;"
+					: "=a" (value)
 					: "d" (port)
 					: );
+	return value;
 }
 
-int port_in_d(int port) {
+uint32_t port_in_d(uint16_t port) {
+	uint32_t value;
 	asm volatile (	"in eax, dx;"
-					:
+					: "=a" (value)
 					: "d" (port)
 					: );
+	return value;
 }

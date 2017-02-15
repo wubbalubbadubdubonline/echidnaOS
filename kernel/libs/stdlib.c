265ca3cbf7acbe46762cbd5a0347fc05f1c9409a
wubbalubbadubdubonline <wubbalubbadubdub94@gmail.com>
2017-02-14 19:03:30 -0600

    long versions of atoi and itoa

--- kernel/libs/stdlib.c
+++ kernel/libs/stdlib.c
@@ -33,6 +33,34 @@ int atoi(const char *str)
 	return res;
 }
 
+long atol(const char *str)
+{
+	long res = 0;
+	int sign = 1;
+
+	while ( isspace(*str) ) str++;
+
+	if ( *str == '-' )
+	{
+		sign = -1;
+		str++;
+	}
+	else if ( *str == '+' )
+	{
+		str++;
+	}
+
+	for ( ; *str; str++ )
+	{
+		if ( isdigit(*str) )
+			res = res * 10 + (*str - '0');
+		else
+			return res;
+	}
+
+	return res;
+}
+
 /*
  * Converts an integer n to a null-terminated string using 
  * the specified base and stores the result in the array 
@@ -43,6 +71,52 @@ char *itoa(int n, char *buf, int base)
 	/* large enough to hold binary ~0 plus the null byte */
 	char tmp[(sizeof(int)*8)+1];
 	unsigned int m;
+
+	char *cp = tmp;
+
+	/* if using base 10, enable negative numbers */
+	if ( base == 10 && n < 0 )
+	{
+		*cp++ = '-';
+		n *= -1;
+	}
+
+	m = n;
+
+	/* create number in reverse for convenience */
+	while ( m )
+	{
+		*cp++ = base_digits[m % base];
+		m /= base;
+	}
+
+	*cp = '\0';
+
+	/* now it's time to copy the sting to buf in proper order */
+	cp = buf;
+
+	if ( base == 10 && *tmp == '-' )
+		*cp++ = *tmp;
+
+	int len = strlen(tmp);
+
+	for ( int i = len-1; i >= 0; i-- )
+		*cp++ = tmp[i];
+
+	/* if the number is negative, then we copied the sign twice */
+	if ( *tmp == '-' )
+		cp--;
+
+	*cp = '\0';
+
+	return buf;
+}
+
+char *ltoa(long n, char *buf, int base)
+{
+	/* large enough to hold binary ~0 plus the null byte */
+	char tmp[(sizeof(long)*8)+1];
+	unsigned long m;
 
 	char *cp = tmp;
 
--- kernel/libs/stdlib.h
+++ kernel/libs/stdlib.h
@@ -3,7 +3,9 @@
 
 
 char *itoa(int, char *, int);
+char *ltoa(long, char *, int);
 int atoi(const char *);
+long atol();
 
 
 #endif

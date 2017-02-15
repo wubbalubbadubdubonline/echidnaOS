ffb1cb5cc8628c9b31c0ab5620546917b5ca7f6a
Mintz98 <the.mintlander@gmail.com>
2017-02-15 20:35:26 +0100

    gcc is broken

--- kernel/drivers/system.h
+++ kernel/drivers/system.h
@@ -16,6 +16,124 @@
 	asm volatile ("sti");					\
 })
 
+#if defined(__32BIT__)		// 32-bit memory operations
+
+#define mem_store_b(address, value) ({				\
+	asm volatile (	"mov byte [ebx], al"			\
+					:								\
+					: "a" (value), "b" (address)	\
+					: );							\
+})
+
+#define mem_store_w(address, value) ({				\
+	asm volatile (	"mov word [ebx], ax"			\
+					:								\
+					: "a" (value), "b" (address)	\
+					: );							\
+})
+
+#define mem_store_d(address, value) ({				\
+	asm volatile (	"mov dword [ebx], eax"			\
+					:								\
+					: "a" (value), "b" (address)	\
+					: );							\
+})
+
+#define mem_load_b(address) ({						\
+	uint8_t value;									\
+	asm volatile (	"mov al, byte [ebx]"			\
+					: "=a" (value)					\
+					: "b" (address)					\
+					: );							\
+	value;											\
+})
+
+#define mem_load_w(address) ({						\
+	uint16_t value;									\
+	asm volatile (	"mov ax, word [ebx]"			\
+					: "=a" (value)					\
+					: "b" (address)					\
+					: );							\
+	value;											\
+})
+
+#define mem_load_d(address) ({						\
+	uint32_t value;									\
+	asm volatile (	"mov eax, dword [ebx]"			\
+					: "=a" (value)					\
+					: "b" (address)					\
+					: );							\
+	value;											\
+})
+
+#elif defined(__64BIT__)	// 64-bit memory operations
+
+#define mem_store_b(address, value) ({				\
+	asm volatile (	"mov byte [rbx], al"			\
+					:								\
+					: "a" (value), "b" (address)	\
+					: );							\
+})
+
+#define mem_store_w(address, value) ({				\
+	asm volatile (	"mov word [rbx], ax"			\
+					:								\
+					: "a" (value), "b" (address)	\
+					: );							\
+})
+
+#define mem_store_d(address, value) ({				\
+	asm volatile (	"mov dword [rbx], eax"			\
+					:								\
+					: "a" (value), "b" (address)	\
+					: );							\
+})
+
+#define mem_store_q(address, value) ({				\
+	asm volatile (	"mov qword [rbx], rax"			\
+					:								\
+					: "a" (value), "b" (address)	\
+					: );							\
+})
+
+#define mem_load_b(address) ({						\
+	uint8_t value;									\
+	asm volatile (	"mov al, byte [rbx]"			\
+					: "=a" (value)					\
+					: "b" (address)					\
+					: );							\
+	value;											\
+})
+
+#define mem_load_w(address) ({						\
+	uint16_t value;									\
+	asm volatile (	"mov ax, word [rbx]"			\
+					: "=a" (value)					\
+					: "b" (address)					\
+					: );							\
+	value;											\
+})
+
+#define mem_load_d(address) ({						\
+	uint32_t value;									\
+	asm volatile (	"mov eax, dword [rbx]"			\
+					: "=a" (value)					\
+					: "b" (address)					\
+					: );							\
+	value;											\
+})
+
+#define mem_load_q(address) ({						\
+	uint64_t value;									\
+	asm volatile (	"mov rax, qword [rbx]"			\
+					: "=a" (value)					\
+					: "b" (address)					\
+					: );							\
+	value;											\
+})
+
+#endif
+
 #define io_wait() ({						\
 	asm volatile (	"out 0x80, al"			\
 					:						\
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -11,34 +11,14 @@
 
 void _start(void) {
 
+	uint16_t x;
+
 	text_clear();
 
-	text_putchar(0x00);
-	text_putstring("echidnaOS\nCrispy with ATA support\n");
-        
-        ata_device ata0m; ata0m = init_ata_device(0x1F0,1); // Primary master
-        text_putstring("ATA primary master: ");
-        ata_identify(ata0m);
-        text_putchar('\n');
-        
-        ata_device ata0s; ata0s = init_ata_device(0x1F0,0); // Primary slave
-        text_putstring("ATA primary slave: ");
-        ata_identify(ata0s);
-        text_putchar('\n');
-        
-        ata_device ata1m; ata1m = init_ata_device(0x170,1); // Secondary master
-        text_putstring("ATA secondary master: ");
-        ata_identify(ata1m);
-        text_putchar('\n');
-        
-        ata_device ata1s; ata1s = init_ata_device(0x170,0); // Secondary slave
-        text_putstring("ATA secondary slave: ");
-        ata_identify(ata1s);
-        text_putchar('\n');
+	text_putstring("echidnaOS\n");
 
-        ata_write28(ata0m, 1, (uint8_t*)0x7c00, 512);
-        ata_flush(ata0m);
-        ata_read28(ata0m, 1, 512);
+	asm("xchg bx,bx");
+	x=mem_load_w(0x7DFE);
 
 	system_halt();
 

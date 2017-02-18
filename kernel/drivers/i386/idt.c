4f5661ae2e5f67b6d960f4c9c54d908d730171d3
Mintz98 <the.mintlander@gmail.com>
2017-02-18 07:54:41 +0100

    thanks chekwob

--- kernel/drivers/i386/idt.c
+++ kernel/drivers/i386/idt.c
@@ -1,23 +1,95 @@
 #include "../idt.h"
 #include "../system.h"
 
-void build_idt(uint32_t table_offset, uint8_t entry_count) {
-	mem_store_w(table_offset, (entry_count*8)-1);	// idt length
-	mem_store_d(table_offset+2, table_offset+6);	// idt start
+#define KERNEL_SELECTOR 0x18
 
-	for (table_offset=table_offset+6; entry_count; entry_count--) {
-		mem_store_d(table_offset, 0x00000000);
-		table_offset=table_offset+4;
-		mem_store_d(table_offset, 0x00000000);
-		table_offset=table_offset+4;
-	}
+char IDT_pointer[6];
+char IDT[0x188];
+
+// ISRs
+void handler_simple(void);
+void handler_code(void);
+void handler_irq_pic0(void);
+void handler_irq_pic1(void);
+void keyboard_isr(void);
+
+// ISRs pointers
+
+void build_pointer(void);
+void build_idt_entry(uint8_t vector, uint16_t selector, uint32_t offset, uint8_t type);
+
+void build_pointer(void) {
+	mem_store_w(IDT_pointer, 0x187);	// idt length
+	mem_store_d(IDT_pointer+2, IDT);	// idt start
+	load_idt(IDT_pointer);				// load the IDT register
 }
 
-void build_idt_entry(uint32_t table_offset, uint8_t vector, uint16_t selector, uint32_t offset, uint8_t type) {
-	table_offset=(vector*8)+(table_offset+6);
-	mem_store_w(table_offset, (offset%0x10000));
-	mem_store_w(table_offset+2, selector);
-	mem_store_b(table_offset+4, 0x00);			// unused
-	mem_store_b(table_offset+5, type);
-	mem_store_w(table_offset+6, (offset/0x10000));
+void build_idt_entry(uint8_t vector, uint16_t selector, uint32_t offset, uint8_t type) {
+	mem_store_w((vector*8)+IDT, offset%0x10000);
+	mem_store_w((vector*8)+IDT+2, selector);
+	mem_store_b((vector*8)+IDT+5, type);
+	mem_store_w((vector*8)+IDT+6, offset/0x10000);
+}
+
+void create_IDT(void) {
+
+	// define handlers' pointers
+	void (*handler_simple_ptr)(void) = handler_simple;
+	void (*handler_code_ptr)(void) = handler_code;
+	void (*handler_irq_pic0_ptr)(void) = handler_irq_pic0;
+	void (*handler_irq_pic1_ptr)(void) = handler_irq_pic1;
+	void (*keyboard_isr_ptr)(void) = keyboard_isr;
+
+	// pointers to uint32_t
+	uint32_t handler_simple_ptr_int = (uint32_t)handler_simple_ptr;
+	uint32_t handler_code_ptr_int = (uint32_t)handler_code_ptr;
+	uint32_t handler_irq_pic0_ptr_int = (uint32_t)handler_irq_pic0_ptr;
+	uint32_t handler_irq_pic1_ptr_int = (uint32_t)handler_irq_pic1_ptr;
+	uint32_t keyboard_isr_ptr_int = (uint32_t)keyboard_isr_ptr;
+
+	build_pointer();		// load the IDTR
+
+	// define interrupt handlers
+
+	build_idt_entry(0x00, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x00, divide by 0
+	build_idt_entry(0x01, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x01, debug
+	build_idt_entry(0x02, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x02, NMI
+	build_idt_entry(0x03, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x03, breakpoint
+	build_idt_entry(0x04, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x04, overflow
+	build_idt_entry(0x05, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x05, bound range exceeded
+	build_idt_entry(0x06, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x06, invalid opcode
+	build_idt_entry(0x07, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x07, device not available
+	build_idt_entry(0x08, KERNEL_SELECTOR, handler_code_ptr_int, 0xee);			// int 0x08, double fault
+	build_idt_entry(0x09, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x09, coprocessor segment overrun
+	build_idt_entry(0x0A, KERNEL_SELECTOR, handler_code_ptr_int, 0xee);			// int 0x0A, invalid TSS
+	build_idt_entry(0x0B, KERNEL_SELECTOR, handler_code_ptr_int, 0xee);			// int 0x0B, segment not present
+	build_idt_entry(0x0C, KERNEL_SELECTOR, handler_code_ptr_int, 0xee);			// int 0x0C, stack-segment fault
+	build_idt_entry(0x0D, KERNEL_SELECTOR, handler_code_ptr_int, 0xee);			// int 0x0D, general protection fault
+	build_idt_entry(0x0E, KERNEL_SELECTOR, handler_code_ptr_int, 0xee);			// int 0x0E, page fault
+	build_idt_entry(0x10, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x10, x87 floating point exception
+	build_idt_entry(0x11, KERNEL_SELECTOR, handler_code_ptr_int, 0xee);			// int 0x11, alignement check
+	build_idt_entry(0x12, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x12, machine check
+	build_idt_entry(0x13, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x13, SIMD floating point exception
+	build_idt_entry(0x14, KERNEL_SELECTOR, handler_simple_ptr_int, 0xee);		// int 0x14, virtualisation exception
+	build_idt_entry(0x1E, KERNEL_SELECTOR, handler_code_ptr_int, 0xee);			// int 0x1E, security exception
+
+	// define IRQ's ISRs
+
+	build_idt_entry(0x20, KERNEL_SELECTOR, handler_irq_pic0_ptr_int, 0xee);		// IRQ 0, PIT
+	build_idt_entry(0x21, KERNEL_SELECTOR, keyboard_isr_ptr_int, 0xee);			// IRQ 1, keyboard
+	build_idt_entry(0x22, KERNEL_SELECTOR, handler_irq_pic0_ptr_int, 0xee);		// IRQ 2
+	build_idt_entry(0x23, KERNEL_SELECTOR, handler_irq_pic0_ptr_int, 0xee);		// IRQ 3, COM2
+	build_idt_entry(0x24, KERNEL_SELECTOR, handler_irq_pic0_ptr_int, 0xee);		// IRQ 4, COM1
+	build_idt_entry(0x25, KERNEL_SELECTOR, handler_irq_pic0_ptr_int, 0xee);		// IRQ 5, LPT2
+	build_idt_entry(0x26, KERNEL_SELECTOR, handler_irq_pic0_ptr_int, 0xee);		// IRQ 6, floppy
+	build_idt_entry(0x27, KERNEL_SELECTOR, handler_irq_pic0_ptr_int, 0xee);		// IRQ 7, LPT1/spurious
+	build_idt_entry(0x28, KERNEL_SELECTOR, handler_irq_pic1_ptr_int, 0xee);		// IRQ 8, CMOS RTC
+	build_idt_entry(0x29, KERNEL_SELECTOR, handler_irq_pic1_ptr_int, 0xee);		// IRQ 9
+	build_idt_entry(0x2A, KERNEL_SELECTOR, handler_irq_pic1_ptr_int, 0xee);		// IRQ 10
+	build_idt_entry(0x2B, KERNEL_SELECTOR, handler_irq_pic1_ptr_int, 0xee);		// IRQ 11
+	build_idt_entry(0x2C, KERNEL_SELECTOR, handler_irq_pic1_ptr_int, 0xee);		// IRQ 12, ps/2 mouse
+	build_idt_entry(0x2D, KERNEL_SELECTOR, handler_irq_pic1_ptr_int, 0xee);		// IRQ 13, FPU
+	build_idt_entry(0x2E, KERNEL_SELECTOR, handler_irq_pic1_ptr_int, 0xee);		// IRQ 14, primary ATA
+	build_idt_entry(0x2F, KERNEL_SELECTOR, handler_irq_pic1_ptr_int, 0xee);		// IRQ 15, secondary ATA
+
 }
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -11,8 +11,6 @@
 #include "drivers/pic.h"
 #include "drivers/idt.h"
 
-#define KERNEL_SELECTOR 0x18
-
 void _start(void) {
 
 	text_clear();
@@ -25,65 +23,10 @@ void _start(void) {
 
 	text_putstring("Building IDT...");
 
-	build_idt(0x80000, 0x81);		// build an 81-entry long IDT at 0x80000
+	create_IDT();		// build the IDT
 
-			// define interrupt handlers
+	enable_ints();		// activate the IDT
 
-	build_idt_entry(0x80000, 0x00, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x00, divide by 0
-	build_idt_entry(0x80000, 0x01, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x01, debug
-	build_idt_entry(0x80000, 0x02, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x02, NMI
-	build_idt_entry(0x80000, 0x03, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x03, breakpoint
-	build_idt_entry(0x80000, 0x04, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x04, overflow
-	build_idt_entry(0x80000, 0x05, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x05, bound range exceeded
-	build_idt_entry(0x80000, 0x06, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x06, invalid opcode
-	build_idt_entry(0x80000, 0x07, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x07, device not available
-	build_idt_entry(0x80000, 0x08, KERNEL_SELECTOR, handler_code, 0xee);		// int 0x08, double fault
-	build_idt_entry(0x80000, 0x09, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x09, coprocessor segment overrun
-	build_idt_entry(0x80000, 0x0A, KERNEL_SELECTOR, handler_code, 0xee);		// int 0x0A, invalid TSS
-	build_idt_entry(0x80000, 0x0B, KERNEL_SELECTOR, handler_code, 0xee);		// int 0x0B, segment not present
-	build_idt_entry(0x80000, 0x0C, KERNEL_SELECTOR, handler_code, 0xee);		// int 0x0C, stack-segment fault
-	build_idt_entry(0x80000, 0x0D, KERNEL_SELECTOR, handler_code, 0xee);		// int 0x0D, general protection fault
-	build_idt_entry(0x80000, 0x0E, KERNEL_SELECTOR, handler_code, 0xee);		// int 0x0E, page fault
-	build_idt_entry(0x80000, 0x0F, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x0F
-	build_idt_entry(0x80000, 0x10, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x10, x87 floating point exception
-	build_idt_entry(0x80000, 0x11, KERNEL_SELECTOR, handler_code, 0xee);		// int 0x11, alignement check
-	build_idt_entry(0x80000, 0x12, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x12, machine check
-	build_idt_entry(0x80000, 0x13, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x13, SIMD floating point exception
-	build_idt_entry(0x80000, 0x14, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x14, virtualisation exception
-	build_idt_entry(0x80000, 0x15, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x15
-	build_idt_entry(0x80000, 0x16, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x16
-	build_idt_entry(0x80000, 0x17, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x17
-	build_idt_entry(0x80000, 0x18, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x18
-	build_idt_entry(0x80000, 0x19, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x19
-	build_idt_entry(0x80000, 0x1A, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x1A
-	build_idt_entry(0x80000, 0x1B, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x1B
-	build_idt_entry(0x80000, 0x1C, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x1C
-	build_idt_entry(0x80000, 0x1D, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x1D
-	build_idt_entry(0x80000, 0x1E, KERNEL_SELECTOR, handler_code, 0xee);		// int 0x1E, security exception
-	build_idt_entry(0x80000, 0x1F, KERNEL_SELECTOR, handler_simple, 0xee);		// int 0x1F
-
-			// define IRQ's ISRs
-
-	build_idt_entry(0x80000, 0x20, KERNEL_SELECTOR, handler_irq_pic0, 0xee);	// IRQ 0, PIT
-	build_idt_entry(0x80000, 0x21, KERNEL_SELECTOR, keyboard_isr, 0xee);		// IRQ 1, keyboard
-	build_idt_entry(0x80000, 0x22, KERNEL_SELECTOR, handler_irq_pic0, 0xee);	// IRQ 2
-	build_idt_entry(0x80000, 0x23, KERNEL_SELECTOR, handler_irq_pic0, 0xee);	// IRQ 3, COM2
-	build_idt_entry(0x80000, 0x24, KERNEL_SELECTOR, handler_irq_pic0, 0xee);	// IRQ 4, COM1
-	build_idt_entry(0x80000, 0x25, KERNEL_SELECTOR, handler_irq_pic0, 0xee);	// IRQ 5, LPT2
-	build_idt_entry(0x80000, 0x26, KERNEL_SELECTOR, handler_irq_pic0, 0xee);	// IRQ 6, floppy
-	build_idt_entry(0x80000, 0x27, KERNEL_SELECTOR, handler_irq_pic0, 0xee);	// IRQ 7, LPT1/spurious
-	build_idt_entry(0x80000, 0x28, KERNEL_SELECTOR, handler_irq_pic1, 0xee);	// IRQ 8, CMOS RTC
-	build_idt_entry(0x80000, 0x29, KERNEL_SELECTOR, handler_irq_pic1, 0xee);	// IRQ 9
-	build_idt_entry(0x80000, 0x2A, KERNEL_SELECTOR, handler_irq_pic1, 0xee);	// IRQ 10
-	build_idt_entry(0x80000, 0x2B, KERNEL_SELECTOR, handler_irq_pic1, 0xee);	// IRQ 11
-	build_idt_entry(0x80000, 0x2C, KERNEL_SELECTOR, handler_irq_pic1, 0xee);	// IRQ 12, ps/2 mouse
-	build_idt_entry(0x80000, 0x2D, KERNEL_SELECTOR, handler_irq_pic1, 0xee);	// IRQ 13, FPU
-	build_idt_entry(0x80000, 0x2E, KERNEL_SELECTOR, handler_irq_pic1, 0xee);	// IRQ 14, primary ATA
-	build_idt_entry(0x80000, 0x2F, KERNEL_SELECTOR, handler_irq_pic1, 0xee);	// IRQ 15, secondary ATA
-
-	load_idt(0x80000);				// load the IDT register
-
-	enable_ints();
 	text_putstring(" Done.\n");
 
 	ata_device* devices = get_ata_devices();

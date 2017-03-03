938c3d4c80be77b868890fb991354084ef39a24d
Mintz98 <the.mintlander@gmail.com>
2017-03-03 18:47:01 +0100

    some stuff

--- kernel/drivers/isr.asm
+++ kernel/drivers/isr.asm
@@ -8,111 +8,139 @@ global syscall
 extern keyboard_handler
 extern text_putchar
 extern text_putstring
+extern keyboard_fetch_char
 
 section .data
 
-cpu_state_esp			dd	0xffbffff0
-interrupted_esp			dd	0
+cpu_state_esp                   dd      0xffbffff0
+interrupted_esp                 dd      0
 
 routine_list:
-	times 0x20	dd	0
-	dd	text_putchar
-	dd	text_putstring
+        times 0x20 dd 0                 ; Reserved syscalls
+        dd      text_putchar
+        dd      text_putstring
+        dd      keyboard_fetch_char
 
 section .text
 
 bits 32
 
 handler_simple:
-	iret
+        iret
 
 handler_code:
-	add esp, 4
-	iret
+        add esp, 4
+        iret
 
 handler_irq_pic0:
-	push eax
-	mov al, 0x20	; acknowledge interrupt to PIC0
-	out 0x20, al
-	pop eax
-	iret
+        push eax
+        mov al, 0x20    ; acknowledge interrupt to PIC0
+        out 0x20, al
+        pop eax
+        iret
 
 handler_irq_pic1:
-	push eax
-	mov al, 0x20	; acknowledge interrupt to both PICs
-	out 0xA0, al
-	out 0x20, al
-	pop eax
-	iret
+        push eax
+        mov al, 0x20    ; acknowledge interrupt to both PICs
+        out 0xA0, al
+        out 0x20, al
+        pop eax
+        iret
 
 keyboard_isr:
-	push eax
-	push ebx
-	push ecx
-	push edx
-	push esi
-	push edi
-	push ebp
-	xor eax, eax
-	in al, 0x60		; read from keyboard
-	push eax
-	call keyboard_handler
-	add esp, 4
-	mov al, 0x20	; acknowledge interrupt to PIC0
-	out 0x20, al
-	pop ebp
-	pop edi
-	pop esi
-	pop edx
-	pop ecx
-	pop ebx
-	pop eax
-	iret
+        push eax
+        push ebx
+        push ecx
+        push edx
+        push esi
+        push edi
+        push ebp
+        push ds
+        push es
+        push fs
+        push gs
+        mov ax, 0x10
+        mov ds, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
+        xor eax, eax
+        in al, 0x60     ; read from keyboard
+        push eax
+        call keyboard_handler
+        add esp, 4
+        mov al, 0x20    ; acknowledge interrupt to PIC0
+        out 0x20, al
+        pop gs
+        pop fs
+        pop es
+        pop ds
+        pop ebp
+        pop edi
+        pop esi
+        pop edx
+        pop ecx
+        pop ebx
+        pop eax
+        iret
 
 syscall:
-	mov dword [interrupted_esp], esp
-	mov esp, dword [cpu_state_esp]
-	push ebx
-	push ecx
-	push edx
-	push esi
-	push edi
-	push ebp
-	mov dword [cpu_state_esp], esp
-	mov esp, dword [interrupted_esp]
-	pop ebx		; EIP
-	pop ecx		; CS
-	pop edx		; FLAGS
-	mov dword [interrupted_esp], esp
-	mov esp, dword [cpu_state_esp]
-	push edx
-	push ecx
-	push ebx
-	mov dword [cpu_state_esp], esp
-	mov esp, dword [interrupted_esp]
-	mov ebx, 4
-	mul ebx
-	sti				; Interrupts enabled
-	call [routine_list+eax]
-	cli				; Interrupts disabled
-	mov dword [interrupted_esp], esp
-	mov esp, dword [cpu_state_esp]
-	pop ebx
-	pop ecx
-	pop edx
-	mov dword [cpu_state_esp], esp
-	mov esp, dword [interrupted_esp]
-	push edx
-	push ecx
-	push ebx
-	mov dword [interrupted_esp], esp
-	mov esp, dword [cpu_state_esp]
-	pop ebp
-	pop edi
-	pop esi
-	pop edx
-	pop ecx
-	pop ebx
-	mov dword [cpu_state_esp], esp
-	mov esp, dword [interrupted_esp]
-	iret
+        mov dword [interrupted_esp], esp
+        mov esp, dword [cpu_state_esp]
+        push ebx
+        push ecx
+        push edx
+        push esi
+        push edi
+        push ebp
+        push ds
+        push es
+        push fs
+        push gs
+        mov bx, 0x10
+        mov ds, bx
+        mov es, bx
+        mov fs, bx
+        mov gs, bx
+        mov dword [cpu_state_esp], esp
+        mov esp, dword [interrupted_esp]
+        pop ebx         ; EIP
+        pop ecx         ; CS
+        pop edx         ; FLAGS
+        mov dword [interrupted_esp], esp
+        mov esp, dword [cpu_state_esp]
+        push edx
+        push ecx
+        push ebx
+        mov dword [cpu_state_esp], esp
+        mov esp, dword [interrupted_esp]
+        mov ebx, 4
+        mul ebx
+        sti                             ; Interrupts enabled
+        call [routine_list+eax]
+        cli                             ; Interrupts disabled
+        mov dword [interrupted_esp], esp
+        mov esp, dword [cpu_state_esp]
+        pop ebx
+        pop ecx
+        pop edx
+        mov dword [cpu_state_esp], esp
+        mov esp, dword [interrupted_esp]
+        push edx
+        push ecx
+        push ebx
+        mov dword [interrupted_esp], esp
+        mov esp, dword [cpu_state_esp]
+        pop gs
+        pop fs
+        pop es
+        pop ds
+        pop ebp
+        pop edi
+        pop esi
+        pop edx
+        pop ecx
+        pop ebx
+        mov dword [cpu_state_esp], esp
+        mov esp, dword [interrupted_esp]
+        iret

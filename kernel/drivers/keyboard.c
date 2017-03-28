3ba8d197d7c85e0424dbb044fee0981c9b819430
wubbalubbadubdubonline <wubbalubbadubdub94@gmail.com>
2017-03-28 14:32:00 -0500

    improve shift key support

--- kernel/drivers/keyboard.c
+++ kernel/drivers/keyboard.c
@@ -1,36 +1,65 @@
 #include "keyboard.h"
 
 static uint8_t capslock_active = 0;
+static uint8_t shift_active = 0;
 static char keyboard_buffer[256];
 static uint16_t buffer_index=0;
 
-const char sc_ascii_upper[] = { '\0', '?', '1', '2', '3', '4', '5', '6',     
-    '7', '8', '9', '0', '-', '=', '\b', '\t', 'Q', 'W', 'E', 'R', 'T', 'Y', 
-        'U', 'I', 'O', 'P', '[', ']', '\n', '\0', 'A', 'S', 'D', 'F', 'G', 
-        'H', 'J', 'K', 'L', ';', '\'', '`', '\0', '\\', 'Z', 'X', 'C', 'V', 
-        'B', 'N', 'M', ',', '.', '/', '\0', '\0', '\0', ' '};
+const char sc_ascii_capslock[] = { '\0', '?', '1', '2', '3', '4', '5', '6',     
+		'7', '8', '9', '0', '-', '=', '\b', '\t', 'Q', 'W', 'E', 'R', 'T', 'Y', 
+				'U', 'I', 'O', 'P', '[', ']', '\n', '\0', 'A', 'S', 'D', 'F', 'G', 
+				'H', 'J', 'K', 'L', ';', '\'', '`', '\0', '\\', 'Z', 'X', 'C', 'V', 
+				'B', 'N', 'M', ',', '.', '/', '\0', '\0', '\0', ' '};
 
-const char sc_ascii_lower[] = { '\0', '?', '1', '2', '3', '4', '5', '6',     
-    '7', '8', '9', '0', '-', '=', '\b', '\t', 'q', 'w', 'e', 'r', 't', 'y', 
-        'u', 'i', 'o', 'p', '[', ']', '\n', '\0', 'a', 's', 'd', 'f', 'g', 
-        'h', 'j', 'k', 'l', ';', '\'', '`', '\0', '\\', 'z', 'x', 'c', 'v', 
-        'b', 'n', 'm', ',', '.', '/', '\0', '\0', '\0', ' '};
+const char sc_ascii_shift[] = { '\0', '?', '!', '@', '#', '$', '%', '^',     
+		'&', '*', '(', ')', '_', '+', '\b', '\t', 'Q', 'W', 'E', 'R', 'T', 'Y', 
+				'U', 'I', 'O', 'P', '{', '}', '\n', '\0', 'A', 'S', 'D', 'F', 'G', 
+				'H', 'J', 'K', 'L', ':', '"', '~', '\0', '|', 'Z', 'X', 'C', 'V', 
+				'B', 'N', 'M', '<', '>', '?', '\0', '\0', '\0', ' '};
+
+const char sc_ascii_shift_capslock[] = { '\0', '?', '!', '@', '#', '$', '%', '^',     
+		'&', '*', '(', ')', '_', '+', '\b', '\t', 'q', 'w', 'e', 'r', 't', 'y', 
+				'u', 'i', 'o', 'p', '{', '}', '\n', '\0', 'a', 's', 'd', 'f', 'g', 
+				'h', 'j', 'k', 'l', ':', '"', '~', '\0', '|', 'z', 'x', 'c', 'v', 
+				'b', 'n', 'm', '<', '>', '?', '\0', '\0', '\0', ' '};
+
+const char sc_ascii_nomod[] = { '\0', '?', '1', '2', '3', '4', '5', '6',     
+		'7', '8', '9', '0', '-', '=', '\b', '\t', 'q', 'w', 'e', 'r', 't', 'y', 
+				'u', 'i', 'o', 'p', '[', ']', '\n', '\0', 'a', 's', 'd', 'f', 'g', 
+				'h', 'j', 'k', 'l', ';', '\'', '`', '\0', '\\', 'z', 'x', 'c', 'v', 
+				'b', 'n', 'm', ',', '.', '/', '\0', '\0', '\0', ' '};
 
 void keyboard_handler(uint8_t input_byte) {
 	char c='\0';
-	if (input_byte == SC_CAPSLOCK || input_byte == SC_LEFT_SHIFT || input_byte == SC_RIGHT_SHIFT || input_byte == SC_LEFT_SHIFT_REL || input_byte == SC_RIGHT_SHIFT_REL) {
+	if ( input_byte == SC_CAPSLOCK )
 		capslock_active = !capslock_active;
-	} else if (buffer_index < 256) {
-		if (input_byte < SC_MAX) {
-			if (capslock_active == 0) {
-				c = sc_ascii_lower[input_byte];
-			} else {
-				c = sc_ascii_upper[input_byte];
-			}
+
+	else if ( input_byte == SC_LEFT_SHIFT || input_byte == SC_RIGHT_SHIFT || input_byte == SC_LEFT_SHIFT_REL || input_byte == SC_RIGHT_SHIFT_REL )
+		shift_active = !shift_active;
+
+	else if (buffer_index < 256)
+	{
+		if (input_byte < SC_MAX)
+		{
+			if ( !capslock_active && ! shift_active )
+				c = sc_ascii_nomod[input_byte];
+
+			else if ( !capslock_active && shift_active )
+				c = sc_ascii_shift[input_byte];
+
+			else if ( capslock_active && shift_active )
+				c = sc_ascii_shift_capslock[input_byte];
+
+			else
+				c = sc_ascii_capslock[input_byte];
+
 			keyboard_buffer[buffer_index] = c;
 			buffer_index++;
 		}
-	} else {
+	}
+
+	else
+	{
 		panic("keyboard buffer overflow");
 	}
 }

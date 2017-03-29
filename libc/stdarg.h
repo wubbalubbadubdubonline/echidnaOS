a71ce05e58728ab3e9faae940638dad8977384d9
wubbalubbadubdubonline <wubbalubbadubdub94@gmail.com>
2017-03-29 02:21:04 -0500

    fixed alignment in varargs

--- libc/stdarg.h
+++ libc/stdarg.h
@@ -3,8 +3,9 @@
 
 
 typedef unsigned char *va_list;
-#define va_start(vl, p) (vl = (((va_list)&p+sizeof(p))))
-#define va_arg(vl, t) (*(t *)((vl += sizeof(t)) - sizeof(t)))
+#define va_start(vl, p) (vl = (((va_list)&p+sizeof(p) > 4 ? sizeof(p) : 4)))
+#define va_arg(vl, t) (*(t *)((vl += sizeof(t) > 4 ? sizeof(t) : 4) \
+                                  - (sizeof(t) > 4 ? sizeof(t) : 4)))
 #define va_copy(vldest, vlsrc) (vldest = vlsrc)
 #define va_end(vl) (vl = NULL)
 
